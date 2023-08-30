51404d367bc7206143dc27f399ac080b26d00948
Jamshid-net <39515083+jamshid-net@users.noreply.github.com>
2023-08-30 14:08:32 +0500

    changes

--- src/Application/UseCases/Categories/Commands/DeleteCategoryCommand.cs
+++ src/Application/UseCases/Categories/Commands/DeleteCategoryCommand.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Application.UseCases.Categories.Commands;
-internal class DeleteCategoryCommand
+public class DeleteCategoryCommand:IRequest
 {
+    public int Id { get; set; }
+}
+public class DeleteCategoryCommandHandler : IRequestHandler<DeleteCategoryCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteCategoryCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var foundCategory = await _context.Categories.FindAsync(new object[] { request.Id }, cancellationToken);
+        if (foundCategory == null)
+            throw new NotFoundException(nameof(Category), request.Id);
+
+        _context.Categories.Remove(foundCategory);
+        await _context.SaveChangesAsync(cancellationToken);
+
+    }
+ 
 }
--- src/Application/UseCases/Categories/Commands/UpdateCategoryCommand.cs
+++ src/Application/UseCases/Categories/Commands/UpdateCategoryCommand.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,27 @@ using System.Threading.Tasks;
 namespace Application.UseCases.Categories.Commands;
 public class UpdateCategoryCommand:IRequest
 {
+    public int Id { get; set; }
+    public string CategoryName { get; set; }
+}
+public class UpdateCategoryCommandHandler : IRequestHandler<UpdateCategoryCommand>
+{
+    private readonly IApplicationDbContext _context;
 
+    public UpdateCategoryCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(UpdateCategoryCommand request, CancellationToken cancellationToken)
+    {
+       var foundCategory =await _context.Categories.FindAsync(new object[] { request.Id }, cancellationToken);
+        if (foundCategory == null)
+            throw new NotFoundException(nameof(Category), request.Id);
+
+        foundCategory.CategoryName = request.CategoryName;
+
+        await _context.SaveChangesAsync(cancellationToken);
+        
+    }
 }
