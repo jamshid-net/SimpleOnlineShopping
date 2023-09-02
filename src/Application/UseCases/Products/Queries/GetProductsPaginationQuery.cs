2021663cbc4d7e3c23403f0fbf32c8c51116730b
Jamshid-net <39515083+jamshid-net@users.noreply.github.com>
2023-09-02 12:25:37 +0500

    some changes

---
+++ src/Application/UseCases/Products/Queries/GetProductsPaginationQuery.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.HelperExtentions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Products.Queries;
+public class GetProductsPaginationQuery:IRequest<Page<Product>>
+{
+
+}
+public class GetProductsPaginationQueryHandler : IRequestHandler<GetProductsPaginationQuery, Page<Product>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetProductsPaginationQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<Page<Product>> Handle(GetProductsPaginationQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
